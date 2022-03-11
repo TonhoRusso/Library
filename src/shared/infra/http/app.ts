ad2ea1d07627410cc03da7a01522e9ee00ec40af
TonhoRusso <m.a.a.rc@hotmail.com>
2022-03-11 00:03:31 -0300

    fix: adjust AppError

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,10 @@
 import 'reflect-metadata';
-import express from 'express';
-
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import { router } from './routes';
 import 'dotenv/config';
+import { AppError } from '@shared/errors/AppError';
 import cors from 'cors';
-
 import '@shared/adapters';
 import '@shared/container';
 
@@ -15,5 +15,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
